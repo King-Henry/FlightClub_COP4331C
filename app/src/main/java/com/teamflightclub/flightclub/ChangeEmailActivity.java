594cbbe807eb22e09a57e786b5acc3aabd97c709
Tommy Goris <goristommyucf@knights.ucf.edu>
2016-11-25 13:05:33 -0500

    Changes to change email activity

--- app/src/main/java/com/teamflightclub/flightclub/ChangeEmailActivity.java
+++ app/src/main/java/com/teamflightclub/flightclub/ChangeEmailActivity.java
@@ -1,11 +1,7 @@
 package com.teamflightclub.flightclub;
 
-import android.support.v7.app.AppCompatActivity;
-
-
 import android.app.AlertDialog;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
