f885b8002c9e7d07d11bcd8b76780321c6882056
Tommy Goris <goristommyucf@knights.ucf.edu>
2016-11-30 18:55:37 -0500

    Added QR creation and QR reading functionality

---
+++ app/src/main/java/com/teamflightclub/flightclub/ReadQRCodeActivity.java
@@ -0,0 +1,13 @@
+package com.teamflightclub.flightclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadQRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_qrcode);
+    }
+}
