9f4eec740dce64e088b3574332fa1b1470238189
carymcewan <cary.mcewan@gmail.com>
2016-11-29 19:18:30 -0500

    Added Car, Hotel, and Reservation classes. Implemented inheritance as well.

---
+++ app/src/main/java/com/teamflightclub/flightclub/Car.java
@@ -0,0 +1,46 @@
+package com.teamflightclub.flightclub;
+
+/**
+ * Created by Carrito on 11/29/2016.
+ */
+
+public class Car extends Reservation{
+
+    String PIID;
+    String supplierName;
+    String carMakeModel;
+    int adultCount;
+    String ratePeriodCode;
+    double unitPrice;
+    double totalPrice;
+    String carClass;
+    int carDoorCount;
+    int largeLuggageCount;
+    String thumbnailUrl;
+    String dropOffCode;
+    String pickupCode;
+
+    public Car() {
+    }
+
+    public Car(String PIID, String supplierName, int adultCount, String carMakeModel, String ratePeriodCode, double unitPrice, double totalPrice, String carClass, int largeLuggageCount, int carDoorCount, String thumbnailUrl, String dropOffCode, String pickupCode) {
+        this.reservationDb = "Cars";
+        this.PIID = PIID;
+        this.reservationId = PIID;
+        this.supplierName = supplierName;
+        this.adultCount = adultCount;
+        this.carMakeModel = carMakeModel;
+        this.ratePeriodCode = ratePeriodCode;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+        this.reservationPrice = totalPrice;
+        this.carClass = carClass;
+        this.largeLuggageCount = largeLuggageCount;
+        this.carDoorCount = carDoorCount;
+        this.thumbnailUrl = thumbnailUrl;
+        this.dropOffCode = dropOffCode;
+        this.reservationLocationCode = dropOffCode;
+        this.pickupCode = pickupCode;
+    }
+
+}
---
+++ app/src/main/java/com/teamflightclub/flightclub/Reservation.java
@@ -0,0 +1,13 @@
+package com.teamflightclub.flightclub;
+
+/**
+ * Created by Carrito on 11/29/2016.
+ */
+
+public class Reservation {
+
+    double reservationPrice;
+    String reservationId;
+    String reservationLocationCode;
+    String reservationDb;
+}
